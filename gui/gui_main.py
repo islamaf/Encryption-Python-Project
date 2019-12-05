9dc9425bd3c9fbbd14da28ff9457fc8a663e34ac
islamaf <44944648+islamaf@users.noreply.github.com>
2019-12-05 18:03:48 +0300

    Update gui_main.py

--- gui/gui_main.py
+++ gui/gui_main.py
@@ -19,14 +19,20 @@ def vigenere_cipher_open():
 def vernam_cipher_open():
     os.system('python vernam_cipher_gui.py')
 
+def atbash_cipher_open():
+    os.system('python atbash_cipher_gui.py')
+
 caesar_button = ttk.Button(root.frame_header, text="Open caesar", command=lambda: caesar_cipher_open())
 caesar_button.grid(row=1, column=1)
 
-caesar_button = ttk.Button(root.frame_header, text="Open vigenere", command=lambda: vigenere_cipher_open())
-caesar_button.grid(row=2, column=1)
+vigenere_button = ttk.Button(root.frame_header, text="Open vigenere", command=lambda: vigenere_cipher_open())
+vigenere_button.grid(row=1, column=2)
 
-caesar_button = ttk.Button(root.frame_header, text="Open vernam", command=lambda: vernam_cipher_open())
-caesar_button.grid(row=3, column=1)
+vernam_button = ttk.Button(root.frame_header, text="Open vernam", command=lambda: vernam_cipher_open())
+vernam_button.grid(row=1, column=3)
+
+atbash_button = ttk.Button(root.frame_header, text="Open atbash", command=lambda: atbash_cipher_open())
+atbash_button.grid(row=1, column=4)
 
 root.frame_header.pack()
 root.mainloop()
